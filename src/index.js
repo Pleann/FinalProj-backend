991c6f7d6079df44ac0d140b5e82661a34692c6d
Plean <plean_som@cmu.ac.th>
2026-05-11 14:15:27 +0700

    bug fixing of UC-01

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const app = require('./app');
+const initDb = require('./util/initDb');
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await initDb();
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on port ${PORT} [${process.env.NODE_ENV}]`);
+        });
+    } catch (err) {
+        console.error('❌ Failed to start:', err);
+        process.exit(1);
+    }
+};
+
+start();
