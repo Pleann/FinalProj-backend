bd61d4b3c33cba4e494c224c32233b59455bbfe3
Plean <plean_som@cmu.ac.th>
2026-06-14 14:27:52 +0700

    added tables to DB
    Added Location, activity, expense tables

--- resources/database/database.sql
+++ resources/database/database.sql
@@ -9,6 +9,11 @@ EXCEPTION WHEN duplicate_object THEN NULL;
 END $$;
 
 DO $$ BEGIN
+CREATE TYPE member_attendance AS ENUM ('VeryEarly', 'Early', 'OnTime', 'Late', 'VeryLate', 'Missing', 'Undecided');
+EXCEPTION WHEN duplicate_object THEN NULL;
+END $$;
+
+DO $$ BEGIN
 CREATE TYPE invite_status AS ENUM ('Accept', 'Reject', 'Cancelled', 'Undecided');
 EXCEPTION WHEN duplicate_object THEN NULL;
 END $$;
@@ -33,7 +38,7 @@ CREATE TABLE IF NOT EXISTS Trip (
                                     meeting_point    VARCHAR(255),
                                     image_url        TEXT,
                                     trip_status      trip_status default 'Upcoming',
-                                    created_by       INTEGER NOT NULL REFERENCES Account(user_id) ON DELETE CASCADE,
+                                    created_by       INTEGER NOT NULL REFERENCES Account(user_id) ON DELETE SET NULL,
 
                                     CONSTRAINT chk_dates CHECK (start_time <= end_time)
 );
@@ -43,6 +48,7 @@ CREATE TABLE IF NOT EXISTS TripMember (
                                           trip_id           INTEGER NOT NULL REFERENCES Trip(trip_id) ON DELETE CASCADE,
                                           user_id           INTEGER NOT NULL REFERENCES Account(user_id) ON DELETE CASCADE,
                                           member_status     member_status default 'Undecided',
+                                          attendance        member_attendance default 'Undecided',
 
                                           CONSTRAINT unique_trip_member UNIQUE (trip_id, user_id)
 );
@@ -54,4 +60,38 @@ CREATE TABLE IF NOT EXISTS TripInvite (
                                           invite_status     invite_status default 'Undecided',
 
                                           CONSTRAINT unique_trip_invite UNIQUE (trip_id, user_id)
+);
+
+CREATE TABLE IF NOT EXISTS Location (
+                                          location_id       SERIAL PRIMARY KEY,
+                                          trip_id           INTEGER NOT NULL REFERENCES Trip(trip_id) ON DELETE CASCADE,
+                                          user_id           INTEGER NOT NULL REFERENCES Account(user_id) ON DELETE CASCADE,
+                                          latitude          DECIMAL(8,6) NOT NULL,
+                                          longitude         DECIMAL(9,6) NOT NULL,
+                                          location_timestamp TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Activity (
+                                          activity_id       SERIAL PRIMARY KEY,
+                                          trip_id           INTEGER NOT NULL REFERENCES Trip(trip_id) ON DELETE CASCADE,
+                                          user_id           INTEGER NOT NULL REFERENCES Account(user_id) ON DELETE CASCADE,
+                                          location_name     VARCHAR(255) NOT NULL,
+                                          location_type     VARCHAR(255) NOT NULL,
+                                          activity_type     VARCHAR(255) NOT NULL,
+                                          ac_start_time     TIMESTAMP NOT NULL,
+                                          ac_end_time       TIMESTAMP NOT NULL,
+
+                                          CONSTRAINT chk_activity_times CHECK (ac_end_time > ac_start_time)
+);
+
+CREATE TABLE IF NOT EXISTS Expense (
+                                          expense_id        SERIAL PRIMARY KEY,
+                                          trip_id           INTEGER NOT NULL REFERENCES Trip(trip_id) ON DELETE CASCADE,
+                                          user_id           INTEGER NOT NULL REFERENCES Account(user_id) ON DELETE CASCADE,
+                                          activity_id       INTEGER REFERENCES Activity(activity_id) ON DELETE CASCADE,
+                                          expense_name      VARCHAR(255) NOT NULL,
+                                          amount            NUMERIC(12,2) NOT NULL,
+                                          currency          CHAR(3),
+                                          billimage_url     TEXT,
+                                          expense_timestamp TIMESTAMP
 );
