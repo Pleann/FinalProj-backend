dc65ad523b4d7af19768f249d94328350b84a50f
teeradui <teerada1107@gmail.com>
2026-07-16 19:59:05 +0700

    miss match entity name

--- src/features/trip/trip.entity.js
+++ src/features/trip/trip.entity.js
@@ -7,8 +7,8 @@ class TripEntity {
         this.startTime = row.start_time;
         this.tripDestination = row.trip_destination;
         this.meetingPointName = row.meeting_point_name;
-        this.meetingPointLat = row.meeting_point.lat;
-        this.meetingPointLon = row.meeting_point.lon;
+        this.meetingPointLat = row.meeting_point_lat;
+        this.meetingPointLon = row.meeting_point_lon;
         this.imageUrl = row.image_url;
         this.tripStatus = row.trip_status;
         this.createdBy = row.created_by;
