bcfd7f8b0e7af8cd5c7952600e8922dc20be1b21
Plean <plean_som@cmu.ac.th>
2026-07-16 12:29:12 +0700

    debug again again

--- src/features/trip/trip.service.js
+++ src/features/trip/trip.service.js
@@ -15,7 +15,7 @@ class TripService {
         }
         const trip = await this.dao.insert(createTripDto, ownerId);
         await this.tripMemberService.addMember(trip.tripId, ownerId, 'Participating');
-        return this.dao.insert(createTripDto, ownerId);
+        return trip;
     }
 
     // async getAllTrips() {
